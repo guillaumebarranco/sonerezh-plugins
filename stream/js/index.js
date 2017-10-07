4c2447179edc2dfefc203341bd971d491cf68ad7
Guillaume Barranco <Guillaume Barranco>
2017-10-08 01:24:31 +0200

    add song play from js

--- stream/js/index.js
+++ stream/js/index.js
@@ -7,7 +7,22 @@ $(function( ){
             method: 'GET',
             success: function(response) {
             	console.log('response', response);
-            	player.play(response.Song.id);
+
+            	var songId = response.Song.id;
+
+            	var songs = [songsManager.getSong(songId)];
+
+		        populatePlaylist(songs);
+		        if (player.isShuffle()) {
+		            player.setFirst(songId);
+		        }
+
+		        if (player.getCurrentTrack().id == songId) {
+		            player.play();
+		        } else {
+		            player.play(songId);
+		        }
+
             }, error: function(err) {
             	console.log('err', err);
             }
