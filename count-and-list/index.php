c188de24c9ece95815de6d04af4f16ec924dfb00
gbarranco <g.barranco@green-alternative.fr>
2017-10-12 16:52:16 +0200

    add count and list test

---
+++ count-and-list/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$path = "/var/music";
+
+$folders = scandir($path):
+
+$totalRappeurs = 0;
+
+foreach ($folders as $folder) {
+
+	$files = scandir($folder);
+
+	foreach ($files as $file) {
+		if($file === ".info") {
+			$info = json_decode(file_get_contents($file));
+			if($info['artist'] && $info['artist']['type'] && $info['artist']['type'] === 'rap') {
+				$totalRappeurs++;
+			}
+		}
+	}
+}
+
+echo "You have ".$totalRappeurs." rappeur";
